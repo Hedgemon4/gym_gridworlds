43e9f252940744a6342deaad1c6e5ed0f82a5940
Seth Akins <sethlinc4@gmail.com>
2025-08-22 10:57:51 -0600

    Added a four rooms variation with no goal

--- gym_gridworlds/__init__.py
+++ gym_gridworlds/__init__.py
@@ -200,6 +200,17 @@ register(
     },
 )
 
+register(
+    id="Gym-Gridworlds/FourRooms-NoGoal-13x13-v0",
+    entry_point="gym_gridworlds.gridworld:Gridworld",
+    max_episode_steps=1,
+    kwargs={
+        "grid": "13x13_four_rooms_no_goal",
+        "no_stay": True,
+        "start_pos": None,  # random
+    },
+)
+
 #### HARD ENVIRONMENTS ####
 
 register(
